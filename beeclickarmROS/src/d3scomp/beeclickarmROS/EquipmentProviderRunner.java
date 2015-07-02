dd0c941c0e5d6135322d34f4b51cdfc345513993
Vladimir Matena <matena@d3s.mff.cuni.cz>
2015-07-02 19:30:53 +0200

    Replace constants by passed parameters when running EquipmentProviderRunner

--- beeclickarmROS/src/d3scomp/beeclickarmROS/EquipmentProviderRunner.java
+++ beeclickarmROS/src/d3scomp/beeclickarmROS/EquipmentProviderRunner.java
@@ -1,6 +1,7 @@
 package d3scomp.beeclickarmROS;
 
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.URI;
 import java.net.URISyntaxException;
 
@@ -14,16 +15,25 @@ import d3scomp.beeclickarmj.JSSCComm;
 
 public class EquipmentProviderRunner {
 	public static void main(String[] args) throws URISyntaxException, CommException, IOException {
+		// Check arguments
+		if(args.length != 1) {
+			System.err.println("Need a name of the serial port top open as a single parameter.");
+			return;
+		}
+		final String serialName = args[0];
+		
+		// ROS parameters
+		final String nodeHost = InetAddress.getLocalHost().getHostName();
+		final URI masterURI = new URI(System.getenv("ROS_MASTER_URI"));
+				
 		// Setup board connection
-		Comm comm = new JSSCComm("COM6");
-
+		Comm comm = new JSSCComm(serialName);
 		comm.start();
-
 		comm.setAddr(0xBABA, 0x0103);
 		comm.setChannel(0);
 		
 		// Common node configuration
-		NodeConfiguration nodeConfig = NodeConfiguration.newPublic("192.168.56.1", new URI("http://192.168.56.101:11311"));
+		NodeConfiguration nodeConfig = NodeConfiguration.newPublic(nodeHost, masterURI);
 		NodeMainExecutor nodeMainExecutor = DefaultNodeMainExecutor.newDefault();
 			
 		// Run ROS SHT1x node
